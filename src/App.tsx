2be958008d628b4ed701b4d3ffc29010cfcd8925
RickaPrincy <rckprincy@gmail.com>
2024-12-19 09:10:40 +0300

    feat: captacha config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages";
+import { HumanVerification, useAwsWafCaptchaHandler } from "./waf";
 
 const App = () => {
+  useAwsWafCaptchaHandler();
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/human-verification" element={<HumanVerification />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,25 @@ import { Box, Button, TextField, Typography } from "@mui/material";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { v4 as uuid } from "uuid";
+import { getAxiosInstance } from "../conf/axios";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router-dom";
 
 const WafFormSchema = z.object({
   N: z.string()
 });
 
 type WafFormType = z.infer<typeof WafFormSchema>;
+const JCLOUDIFY_URL = "https://api.prod.jcloudify.com/whoami";
 export const Home = () => {
-  const [countConf, setCountConf] = useState<{ maxCount: number, isDoingSequence: boolean, current: number }>({
+  const navigate = useNavigate();
+  const [countConf, setCountConf] = useState<{ timer: NodeJS.Timeout | null, maxCount: number, isDoingSequence: boolean, current: number }>({
     current: 0,
     isDoingSequence: false,
-    maxCount: 0
+    maxCount: 0,
+    timer: null
   });
 
   const { register, handleSubmit } = useForm<WafFormType>({
@@ -25,15 +31,48 @@ export const Home = () => {
   });
 
   const doSequence = (data: WafFormType) => {
+    const timer = setInterval(async () => {
+      whoami();
+    }, 1_000);
+
     setCountConf({
       current: 0,
       isDoingSequence: true,
-      maxCount: +data.N
+      maxCount: +data.N,
+      timer
     });
   }
 
-  const logs = Array(countConf.maxCount).fill(0);
+  const whoami = async () => {
+    try {
+      await getAxiosInstance().get(JCLOUDIFY_URL);
+      throw new Error("Expected error");
+    } catch (error) {
+      if ((error as AxiosError).status === 405) {
+        navigate(`/human-verification?current=${countConf.current}&max=${countConf.maxCount}`);
+        return;
+      }
 
+      setCountConf(prev => ({
+        ...prev,
+        current: prev.current + 1
+      }));
+    }
+  }
+
+  useEffect(() => {
+    if (countConf.current >= countConf.maxCount) {
+      clearInterval(countConf.timer!);
+      setCountConf({
+        current: 0,
+        timer: null,
+        maxCount: 1,
+        isDoingSequence: false
+      })
+    }
+  }, [countConf.current, countConf.maxCount]);
+
+  const logs = Array(countConf.current).fill(0);
   return (
     <Box sx={{ mx: "auto", width: "fit-content" }}>
       <Typography sx={{ textAlign: "center", fontSize: "1rem", opacity: .8, fontWeight: "bold", mt: 5, mb: 2 }}>
